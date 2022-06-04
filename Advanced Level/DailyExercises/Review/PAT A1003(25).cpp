6515b5882504c107e79908df8301ad977d9c26bc
Lunan <834946595@qq.com>
2022-06-04 22:49:16 +0800

    Update PAT A1003(25).cpp

--- Advanced Level/DailyExercises/Review/PAT A1003(25).cpp
+++ Advanced Level/DailyExercises/Review/PAT A1003(25).cpp
@@ -1,111 +1,96 @@
 #include <cstdio>
-#include <string>
-#include <iostream>
 #include <algorithm>
-#include <queue>
+
 using namespace std;
+ 
 
+const int INF = 1000000;
+const int maxn = 501;
 
-const int maxn = 11;
+int G[maxn][maxn];
 
+int N, M, C1, C2;
 
-struct Node{
-	int id;
-	int left;
-	int right;
-}nodes[maxn];
+int weight[maxn];
 
-int N;
+//记录最短路径 
+int d[maxn];
 
-bool isRoot[maxn] = {false};
+//记录是否访问过
+bool visit[maxn] = {false};
 
-void postOrder(int root){
-	if(root == -1){
-		return;
-	}
-	postOrder(nodes[root].left);
-	postOrder(nodes[root].right);
-	swap(nodes[root].left,nodes[root].right);
-}
+//记录路径的数量以及最大的点权和 
+int num[maxn], w[maxn]; 
 
-int num2 = 0;
-void inOrder(int root){
-	if(root == -1){
-		return;
-	}
-	inOrder(nodes[root].left);
-
-	printf("%d",root);
-	num2++;
-	if(num2 < N){
-		printf(" ");
-	}else{
-		printf("\n");
-	}
-	inOrder(nodes[root].right);
-}
-void BFS(int root){
-	queue<int> q;
-	q.push(root);
-	int count = 0;
-	while(!q.empty()){
-		int front = q.front();
-		q.pop();
-		printf("%d",front);
-		count++;
-		if(count < N){
-			printf(" ");
-		}else{
-			printf("\n");
+ 
+void Dijkstra(int s){
+	fill(num,num+maxn,0);
+	fill(w,w+maxn,0);
+	fill(d,d+maxn,INF);
+	
+	d[s] = 0;
+	w[s] = weight[s];
+	num[s] = 1;
+	
+	
+	 
+	for(int i = 0; i < N;i++){
+		//从 V-S中找 
+		int u = -1,min = INF;
+		for(int j = 0; j < N;j++){
+			if(!visit[j] && d[j] < min){
+				min = d[j];
+				u = j;
+			} 
 		}
-		if(nodes[front].left != -1 ){
-			q.push(nodes[front].left);
-		}
-		if(nodes[front].right != -1){
-			q.push(nodes[front].right);
-		}
+		//如果找不到这样的点
+		if(u == -1){
+			return ;
+		} 
+		//访问u
+		visit[u] = true; 
+		//找到后，更新
+		for(int j = 0; j < N; j++){
+			//如果没用访问且有边存在 
+			if(!visit[j] && G[u][j] != INF){
+				if(d[u] + G[u][j] < d[j]){
+					//找到更短的路径的时候 
+					d[j] = d[u] + G[u][j];
+					//点权和  
+					w[j] = weight[j] + w[u];
+					//路径数量
+					num[j] = num[u]; 
+				}else if(d[u] + G[u][j] == d[j]){
+					//路径相等时候 
+					if(w[u] + weight[j] > w[j]){
+						w[j] = w[u] + weight[j];	
+					}
+					//路径条数
+					num[j] = num[j] + num[u]; 
+				}
+			}
+
+		} 
+		
+		
 	}
 }
 
 int main(){
-	scanf("%d",&N);
-	string data;
-	for(int i = 0; i < N; i++){
-		nodes[i].id = i;
+	scanf("%d%d%d%d",&N,&M,&C1,&C2);
+	for(int i = 0; i < N;i++){
+		scanf("%d",&weight[i]);
 	}
-	fill(isRoot,isRoot + N, true);
-	for(int i = 0; i < N; i++){
-		cin >> data;
-		int temp_data;
-		if(data == "-"){
-			temp_data = -1;
-		}else{
-			temp_data = stoi(data);
-			isRoot[temp_data] = false;
-		}
-		nodes[i].left = temp_data;
-	//	printf("left:%d ",temp_data);
-		getchar();
-		cin >> data;
-		if(data == "-"){
-			temp_data = -1;
-		}else{
-			temp_data = stoi(data);
-			isRoot[temp_data] = false;
-		}
-		nodes[i].right = temp_data;
-	//	printf(" right:%d\n",temp_data);
+	//初始化G
+	fill(G[0],G[0] + maxn * maxn, INF);
+	int a,b,l;
+	for(int i = 0; i < M; i++){
+		scanf("%d%d%d",&a,&b,&l);
+		G[a][b] = l;
+		G[b][a] = l;
 	}
-	
-	int root ;
-	for(int i = 0; i < N; i++){
-		if(isRoot[i] == true){
-			root = i;
-			break;
-		}
-	}
-	postOrder(root);
-	BFS(root);
-	inOrder(root);
+	//从C1开始的单源最短路径 
+	Dijkstra(C1); 
+	printf("%d %d\n",num[C2],w[C2]);
 	return 0;
-}
+} 
